bff80a3a5dec160d321f304b5ae112342ee3a40a
Togrul <78154535+togrulmemmedov@users.noreply.github.com>
2022-04-17 17:30:28 +0400

    Update main.js

--- main.js
+++ main.js
@@ -37,8 +37,8 @@ function removeList(list_id){
 }
 // sorting list functionality
 function sorList(){
-     if(document.querySelector("img").src=="/images/drop-down-hover.svg"){
-         document.querySelector("img").src="/images/drop-up-hover.svg";
+     if(document.querySelector("img").src=="https://mellow-platypus-16e2ff.netlify.app/images/drop-down-hover.svg"){
+         document.querySelector("img").src="https://mellow-platypus-16e2ff.netlify.app/images/drop-up-hover.svg";
       
         list.innerHTML='';
         toDoList.sort();
@@ -50,7 +50,7 @@ function sorList(){
         }
      }
      else{
-        document.querySelector("img").src="/images/drop-down-hover.svg";
+        document.querySelector("img").src="https://mellow-platypus-16e2ff.netlify.app/images/drop-down-hover.svg";
         toDoList.sort().reverse();
         list.innerHTML='';
          for(let i = 0;i<toDoList.length;i++){
